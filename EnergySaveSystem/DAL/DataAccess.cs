a23b92242a48dc794e0318868b83b508d9dae82c
yehongjun <h0149721@fihbj.cn>
2021-11-25 19:31:23 +0800

    first commit

---
+++ EnergySaveSystem/DAL/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergySaveSystem.DAL
+{
+    public class DataAccess
+    {
+        string dbConfig;
+        MySqlConnection connection;
+        MySqlCommand command;
+        MySqlDataAdapter adapter;
+        MySqlTransaction trans;
+    }
+}
