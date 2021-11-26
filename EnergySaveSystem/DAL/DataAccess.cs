3e8a797fddaaa01c191b423c84dcc4fe64172907
yehongjun <h0149721@fihbj.cn>
2021-11-26 17:28:01 +0800

    2021.11.26-P5

--- EnergySaveSystem/DAL/DataAccess.cs
+++ EnergySaveSystem/DAL/DataAccess.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,77 @@ namespace EnergySaveSystem.DAL
 {
     public class DataAccess
     {
-        string dbConfig;
+        string dbConfig = ConfigurationManager.ConnectionStrings["db_config"].ToString();
         MySqlConnection connection;
         MySqlCommand command;
         MySqlDataAdapter adapter;
         MySqlTransaction trans;
+
+        //资源释放
+        private void Dispose()
+        {
+            if(trans!=null)
+            {
+                trans.Dispose();
+                trans = null;
+            }
+            if (adapter != null)
+            {
+                adapter.Dispose();
+                adapter = null;
+            }
+            if (command != null)
+            {
+                command.Dispose();
+                command = null;
+            }
+            if (connection != null)
+            {
+                connection.Close();
+                connection.Dispose();
+                connection = null;
+            }
+        }
+
+        private DataTable GetData(string sql)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                connection = new MySqlConnection(dbConfig);
+                connection.Open();
+                adapter = new MySqlDataAdapter(sql, connection);
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                this.Dispose();
+            }
+            return dt;
+        }
+
+        public DataTable GetStorageArea()
+        {
+            string stringSql = "";
+            return GetData(stringSql);
+        }
+
+        public DataTable GetDevice()
+        {
+            string stringSql = "";
+            return GetData(stringSql);
+        }
+
+        public DataTable GetMonitorValues()
+        {
+            string stringSql = "";
+            return GetData(stringSql);
+        }
+
     }
 }
