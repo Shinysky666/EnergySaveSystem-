b8d0e9210e0488447335fcfdd1618f830fed5b46
yehongjun <h0149721@fihbj.cn>
2021-12-12 12:09:46 +0800

    2021.12.12-P25

--- EnergySaveSystem/App.xaml.cs
+++ EnergySaveSystem/App.xaml.cs
@@ -17,8 +17,10 @@ namespace EnergySaveSystem
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            new MainWindow().Show();
 
-            GlobalMonitor.Start(
+
+            /*GlobalMonitor.Start(
                 () =>
                 {
                     Application.Current.Dispatcher.Invoke(() =>
@@ -26,7 +28,7 @@ namespace EnergySaveSystem
                         new MainWindow().Show();
                     });
                 },
-                (msg) => 
+                (msg) =>
                 {
                     Application.Current.Dispatcher.Invoke(() =>
                     {
@@ -34,7 +36,7 @@ namespace EnergySaveSystem
                         MessageBox.Show(msg, "系统启动失败");
                         Application.Current.Shutdown();
                     });
-                });
+                });*/
         }
     }
 }
---
+++ EnergySaveSystem/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using EnergySaveSystem.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EnergySaveSystem.ViewModel
+{
+    public class MainWindowViewModel: NotifyPropertyBase
+    {
+        private UIElement _mainContent; 
+
+        public UIElement MainContent
+        {
+            get { return _mainContent; }
+            set { Set<UIElement>(ref _mainContent, value); }
+        }
+
+        public CommandBase TabChangedCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            //MainContent事件绑定
+            TabChangedCommand = new CommandBase(OnTabChanged);
+            //初始化 MainContent
+            OnTabChanged("EnergySaveSystem.View.SystemMonitor");
+        }
+
+        private void OnTabChanged(object obj)
+        {
+            if (obj == null)
+                return;
+
+            //完整方式
+            /*string[] strValues = o.ToString().Split('|');
+            Assembly assembly = Assembly.LoadFrom(strValues[0]);
+            Type type = assembly.GetType(strValues[1]);*/
+
+            //简化方式 必须在同一程序集下
+            Type type = Type.GetType(obj.ToString());
+            this.MainContent = (UIElement)Activator.CreateInstance(type);
+        }
+    }
+}
