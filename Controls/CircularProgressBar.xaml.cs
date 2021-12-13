78d395d6d61e25602cfaf2bc84f2e16eb9959113
yehongjun <h0149721@fihbj.cn>
2021-12-13 19:14:12 +0800

    2021.12.13-P26

--- Controls/CircularProgressBar.xaml.cs
+++ Controls/CircularProgressBar.xaml.cs
@@ -18,9 +18,9 @@ namespace Controls
     /// <summary>
     /// UserControl1.xaml 的交互逻辑
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class CircularProgressBar : UserControl
     {
-        public UserControl1()
+        public CircularProgressBar()
         {
             InitializeComponent();
         }
