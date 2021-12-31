c0ea413ad0406fa002ac3d383238e5fb9f713e4c
yehongjun <h0149721@fihbj.cn>
2021-12-31 17:54:56 +0800

    2021.12.31-P49

--- Controls/Components/CoolingTower.xaml.cs
+++ Controls/Components/CoolingTower.xaml.cs
@@ -24,5 +24,47 @@ namespace Controls.Components
         {
             InitializeComponent();
         }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            { 
+                _isSelected = value;
+                VisualStateManager.GoToState(this, value ? "SelectedState" : "UnSelectedState", false);
+            }
+        }
+
+        public bool IsRuning
+        {
+            get { return (bool)GetValue(IsRuningProperty); }
+            set { SetValue(IsRuningProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsRuningProperty =
+            DependencyProperty.Register("IsRuning", typeof(bool), typeof(CoolingTower),
+                new PropertyMetadata(default(bool), new PropertyChangedCallback(OnRuningStateChanged)));
+        public static void OnRuningStateChanged(DependencyObject o,DependencyPropertyChangedEventArgs e)
+        {
+            bool state = (bool)e.NewValue;
+            VisualStateManager.GoToState(o as CoolingTower, state ? "RunState" : "StopState", false);
+        }
+
+        public bool IsWarning
+        {
+            get { return (bool)GetValue(IsWarningProperty); }
+            set { SetValue(IsWarningProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsWarningProperty =
+            DependencyProperty.Register("IsWarning", typeof(bool), typeof(CoolingTower),
+                new PropertyMetadata(default(bool), new PropertyChangedCallback(OnWarningStateChanged)));
+        public static void OnWarningStateChanged(DependencyObject o,DependencyPropertyChangedEventArgs e)
+        {
+            bool state = (bool)e.NewValue;
+            VisualStateManager.GoToState(o as CoolingTower, state ? "WarningState" : "NormalState", false);
+        }
+
     }
 }
