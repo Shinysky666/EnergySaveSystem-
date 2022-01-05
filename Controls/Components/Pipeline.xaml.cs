e9e845b639ac4ba65377c4f8d6ca07dbd6904dec
yehongjun <h0149721@fihbj.cn>
2022-01-05 19:06:15 +0800

    2022.1.5-P53

--- Controls/Components/Pipeline.xaml.cs
+++ Controls/Components/Pipeline.xaml.cs
@@ -20,9 +20,51 @@ namespace Controls.Components
     /// </summary>
     public partial class Pipeline : UserControl
     {
+
+        //默认管道流向从E(西)往W(东)
+        public int Direction
+        {
+            get { return (int)GetValue(DirectionProperty); }
+            set { SetValue(DirectionProperty, value); }
+        }
+        public static readonly DependencyProperty DirectionProperty =
+            DependencyProperty.Register("Direction", typeof(int), typeof(Pipeline), 
+                new PropertyMetadata(default(int),new PropertyChangedCallback(OnDirectionChanged)));
+        private static void OnDirectionChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            int value = int.Parse(e.NewValue.ToString());
+            VisualStateManager.GoToState(o as Pipeline, value == 1 ? "WEFlowState" : "EWFlowState", false);
+        }
+
+        //管道流水颜色
+        public Brush liquidColor
+        {
+            get { return (Brush)GetValue(liquidColorProperty); }
+            set { SetValue(liquidColorProperty, value); }
+        }
+        public static readonly DependencyProperty liquidColorProperty =
+            DependencyProperty.Register("liquidColor", typeof(Brush), typeof(Pipeline), 
+                new PropertyMetadata(Brushes.Orange));
+
+        //管道圆角
+        public int CapRadius
+        {
+            get { return (int)GetValue(CapRadiusProperty); }
+            set { SetValue(CapRadiusProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CapRadius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CapRadiusProperty =
+            DependencyProperty.Register("CapRadius", typeof(int), typeof(Pipeline), 
+                new PropertyMetadata(default(int)));
+
+
+
+
         public Pipeline()
         {
             InitializeComponent();
         }
+
     }
 }
